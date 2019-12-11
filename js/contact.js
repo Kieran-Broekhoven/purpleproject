06b895caa1d2b1723163e95eebcb231e941e4113
Kieran Broekhoven <kbroekho@qti.qualcomm.com>
2019-12-11 10:02:06 -0800

    Fixed contact form

--- js/contact.js
+++ js/contact.js
@@ -13,45 +13,38 @@ $(document).ready(function(){
         $('#contactForm').validate({
             rules: {
                 name: {
-                    required: true,
-                    minlength: 2
+                    required: true
                 },
                 subject: {
-                    required: true,
-                    minlength: 4
+                    required: true
                 },
                 number: {
-                    required: true,
-                    minlength: 5
+                    required: false
                 },
                 email: {
                     required: true,
                     email: true
                 },
                 message: {
-                    required: true,
-                    minlength: 20
+                    required: true
                 }
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Please include your name"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Please include a subject"
                 },
                 number: {
                     required: "come on, you have a number, don't you?",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Please enter your email address"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Please enter a message",
                 }
             },
             submitHandler: function(form) {
