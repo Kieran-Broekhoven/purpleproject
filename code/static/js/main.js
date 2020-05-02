53e72a14e9149cf67355a1db443fcd5a229ffed4
Kim <kaforney6686@gmail.com>
2020-05-02 15:48:22 -0700

    Homepage Video
    heeelp ;) I emailed you the resources I found including the one I used to build mine.

--- code/static/js/main.js
+++ code/static/js/main.js
@@ -35,6 +35,14 @@ $(document).ready(function(){
       openedSymbol:'-'
     });
   };
+    
+    $(document).ready(function() {
+  $('#headerVideoLink').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });         
+});
+    
 // blog-menu
   // $('ul#blog-menu').slicknav({
   //   prependTo: ".blog_menu"
